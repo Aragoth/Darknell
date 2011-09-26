2d96cf98c5ded3e3729d84daa4940ad06326844b
Aragoth <tharnid@gmail.com>
2011-09-25 22:25:16 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Darknell::Application.config.secret_token = '3f31741fdf92e7c6aabb5e332e7ea38295a3456a8826576db35223387be1f7e3f83eded6e63d90f26f0c57f5cbd72489bf2ad6eae89f8b1ef340170a7a1ea602'
