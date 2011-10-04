ebd3dc4824fcbd359040ce7333297b605a58de29
Aragoth <tharnid@gmail.com>
2011-10-03 22:55:52 -0400

    Finished the Ruby Flavor

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+
   def home
     @title = "Home"
   end
@@ -6,13 +7,12 @@ class PagesController < ApplicationController
   def contact
     @title = "Contact"
   end
-
+  
   def about
     @title = "About"
   end
-
+  
   def help
     @title = "Help"
   end
-
 end
